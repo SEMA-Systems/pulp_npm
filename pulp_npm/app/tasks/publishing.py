d99c8f79336be3a7a1b1efe67f5c841202bb0c3b
Sebastian Lipp <sebastian.lipp@sema-systems.at>
2025-03-27 00:20:02 +0000

    Remove unneeded package object from publishing task

--- pulp_npm/app/tasks/publishing.py
+++ pulp_npm/app/tasks/publishing.py
@@ -7,12 +7,11 @@ log = logging.getLogger(__name__)
 
 def publish(repository_pk, package_pk):
     repository = NpmRepository.objects.get(pk=repository_pk)
-    package = Package.objects.get(pk=package_pk)
     distribution = NpmDistribution.objects.get(repository=repository)
 
     # create new repository version
     with repository.new_version(base_version=repository.latest_version()) as new_version:
-        new_version.add_content(Package.objects.filter(pk=package.pk))
+        new_version.add_content(Package.objects.filter(pk=package_pk))
 
     # switch distribution to new version
     distribution.repository_version = new_version
