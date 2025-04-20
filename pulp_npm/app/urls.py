4ebbcf04d32c002cf45c9318392ff1a0338dc709
Sebastian Lipp <sebastian.lipp@sema-systems.at>
2025-04-20 23:55:31 +0000

    Allow upload of scoped npm packages

--- pulp_npm/app/urls.py
+++ pulp_npm/app/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, re_path
 from .viewsets import NpmUserLoginView, PackageViewSet
 
 urlpatterns = [
     # NPM user login endpoint
-    path('pulp/api/v3/npm/cli/<str:reponame>/-/user/org.couchdb.user:<str:username>', NpmUserLoginView.as_view(), name='npm-user-login'),
+    path(
+        'pulp/api/v3/npm/cli/<str:reponame>/-/user/org.couchdb.user:<str:username>',
+        NpmUserLoginView.as_view(),
+        name='npm-user-login',
+    ),
 
-    # NPM package upload endpoint
-    path('pulp/api/v3/npm/cli/<str:reponame>/<str:packagename>/', PackageViewSet.as_view(({'put': 'create'})), name='npm-package-upload'),
+    # NPM scoped/unscoped package upload
+    re_path(
+        r'^pulp/api/v3/npm/cli/(?P<reponame>[^/]+)/(?P<packagename>.+)/$',
+        PackageViewSet.as_view({'put': 'create'}),
+        name='npm-package-upload',
+    ),
 ]
